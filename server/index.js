2bd0f0681608ed378f2964cbf40c0f97c975a060
Arcival Palma <palma_arcival@yahoo.com>
2022-01-27 22:43:40 +0000

    setup server and database

---
+++ server/index.js
@@ -0,0 +1,34 @@
+// load .env data into process.env
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const morgan = require("morgan");
+
+// Database
+const db = require("./models");
+
+// colors for logging the endPoints
+app.use(morgan("dev"));
+
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  next();
+});
+
+app.use(express.json()); // for parsing application/json
+
+const PORT = process.env.PORT || 8080;
+
+db.sequelize
+  .sync()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
