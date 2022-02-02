392a528559c55a82097d7ccf82fed1203b3b5516
Neha Yadav <nehayadav903@gmail.com>
2022-02-02 12:21:29 -0500

    set routes for comments

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -1,5 +1,7 @@
 const model = require("../models");
 const Comment = model.comment;
+const User = model.user;
+const Image = model.image;
 
 module.exports = {
   createComment: async (req, res) => {
@@ -7,14 +9,13 @@ module.exports = {
       const user = req.user;
 
       const { projectId } = req.params;
-      const { body , commentId} = req.validatedBody;
-      
+      const { body, commentId } = req.validatedBody;
 
       const newComment = await Comment.create({
         userId: user.id,
         projectId: projectId,
         body: body,
-        commentId: commentId
+        commentId: commentId,
       });
 
       return res.status(200).send({
@@ -26,6 +27,102 @@ module.exports = {
       return res.status(400).send({
         success: false,
         error: error.message,
+      });
+    }
+  },
+
+  getAllComments: async (req, res) => {
+    try {
+      const comments = await Comment.findAll({
+        where: {
+          projectId: req.params.projectId,
+        },
+        include: [
+          {
+            model: User,
+            attributes: {
+              exclude: ["password"],
+            },
+            include: [
+              {
+                model: Image,
+              },
+            ],
+          },
+          {
+            model: Comment,
+            as: "reply",
+            include: [
+              {
+                model: User,
+                attributes: {
+                  exclude: ["password"],
+                },
+                include: [
+                  {
+                    model: Image,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+
+      return res.status(200).send({
+        success: true,
+        comments,
+      });
+    } catch (error) {
+      console.log(error.message);
+
+      return res.status(400).send({
+        success: false,
+        error: error.message,
+      });
+    }
+  },
+
+  updateComment: async (req, res) => {
+    try {
+      const comment = req.comment;
+
+      if (!comment) throw new Error("Comment not found");
+
+      comment.body = req.validatedBody.body;
+
+      await comment.save();
+
+      return res.status(200).send({
+        success: true,
+      });
+    } catch (error) {
+      console.log(error.name);
+
+      return res.status(400).send({
+        success: false,
+        error: error.name,
+      });
+    }
+  },
+
+  deleteComment: async (req, res) => {
+    try {
+      const comment = req.comment;
+
+      if (!comment) throw new Error("Comment not found");
+
+      await comment.destroy();
+
+      return res.status(200).send({
+        success: true,
+      });
+    } catch (error) {
+      console.log(error.name);
+
+      return res.status(400).send({
+        success: false,
+        error: error.name,
       });
     }
   },
--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const { userAuth } = require("../middlewares/user.middleware");
 
 const { validate } = require("../validators");
-const{createCommentSchema} =require("../validators/comment.validator");
-const {checkProjectMiddleware} = require("../middlewares/project.middleware")
-const {createComment}= require("../controllers/comment.controller")
+const { createCommentSchema } = require("../validators/comment.validator");
 
-router.route("/:projectId").post(userAuth,validate(createCommentSchema), checkProjectMiddleware, createComment);
+const { userAuth } = require("../middlewares/user.middleware");
+const { checkProjectMiddleware } = require("../middlewares/project.middleware");
+const { commentMiddleware } = require("../middlewares/comment.middleware");
 
+const {
+  createComment,
+  getAllComments,
+  updateComment,
+  deleteComment,
+} = require("../controllers/comment.controller");
+
+router
+  .route("/:projectId")
+  .get(checkProjectMiddleware, getAllComments)
+  .post(
+    userAuth,
+    validate(createCommentSchema),
+    checkProjectMiddleware,
+    createComment
+  );
+
+router
+  .route("/:commentId")
+  .put(
+    userAuth,
+    validate(createCommentSchema),
+    checkProjectMiddleware,
+    commentMiddleware,
+    updateComment
+  )
+  .delete(userAuth, commentMiddleware, deleteComment);
 
 module.exports = router;
