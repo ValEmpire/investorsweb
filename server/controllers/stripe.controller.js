69211106459f49cd6fe2b8abc29dfd5b2436727c
Arcival Palma <palma_arcival@yahoo.com>
2022-02-10 16:52:26 +0000

    add stripe routes in server add, delete, update

--- server/controllers/stripe.controller.js
+++ server/controllers/stripe.controller.js
@@ -40,10 +40,102 @@ module.exports = {
         object: "card",
       });
 
+      let cards = [];
+
+      let primaryCard = {};
+
+      for (const card of account.data) {
+        if (card.default_for_currency) {
+          primaryCard = card;
+        } else {
+          cards.push(card);
+        }
+      }
+
       return res.status(200).send({
         success: true,
-        cards: account.data,
+        cards,
+        primaryCard,
       });
+    } catch (err) {
+      console.log(err.message);
+
+      return res.status(400).send({
+        success: false,
+        error: err.message,
+      });
+    }
+  },
+
+  addCard: async (req, res) => {
+    try {
+      const { stripeId } = req.user;
+
+      const { number, expiry, cvc, name } = req.body;
+
+      const exp_month = expiry.substring(0, 2);
+
+      const exp_year = expiry.substring(2, 4);
+
+      const token = await stripe.tokens.create({
+        card: {
+          number,
+          exp_month,
+          exp_year,
+          cvc,
+          name,
+          currency: "CAD",
+        },
+      });
+
+      const card = await stripe.accounts.createExternalAccount(stripeId, {
+        external_account: token.id,
+      });
+
+      return res.status(200).send({
+        success: true,
+        card,
+      });
+    } catch (err) {
+      console.log(err);
+
+      return res.status(400).send({
+        success: false,
+        error: err.message,
+      });
+    }
+  },
+
+  deleteCard: async (req, res, next) => {
+    try {
+      const { stripeId } = req.user;
+
+      const { cardId } = req.body;
+
+      await stripe.accounts.deleteExternalAccount(stripeId, cardId);
+
+      next();
+    } catch (err) {
+      console.log(err.message);
+
+      return res.status(400).send({
+        success: false,
+        error: err.message,
+      });
+    }
+  },
+
+  updateCard: async (req, res, next) => {
+    try {
+      const { stripeId } = req.user;
+
+      const { cardId } = req.body;
+
+      await stripe.accounts.updateExternalAccount(stripeId, cardId, {
+        default_for_currency: true,
+      });
+
+      next();
     } catch (err) {
       console.log(err.message);
 
--- server/routes/stripe.route.js
+++ server/routes/stripe.route.js
@@ -10,11 +10,20 @@ const { userAuth } = require("../middlewares/user.middleware");
 const {
   createConnectedAccount,
   getAllCards,
+  addCard,
+  deleteCard,
+  updateCard,
 } = require("../controllers/stripe.controller");
 
 //ROUTES
 router.route("/all-cards").get(userAuth, getAllCards);
 
 router.route("/create-account").post(userAuth, createConnectedAccount);
+
+router.route("/add-card").post(userAuth, addCard);
+
+router.route("/delete-card").delete(userAuth, deleteCard, getAllCards);
+
+router.route("/update-card").put(userAuth, updateCard, getAllCards);
 
 module.exports = router;
