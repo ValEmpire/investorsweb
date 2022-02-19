e52d6ca563bfeb7827f925f9d961f625c1a7bf6b
Arcival Palma <palma_arcival@yahoo.com>
2022-02-19 04:44:12 +0000

    change icon of avatar listitem

--- client/src/layout/navbar/AvatarPopper.js
+++ client/src/layout/navbar/AvatarPopper.js
@@ -1,14 +1,25 @@
 import React from "react";
 import {
+  Avatar,
   Box,
   Card,
   ClickAwayListener,
   Divider,
   Fade,
   Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
   Popper,
   Typography,
 } from "@mui/material";
+
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import BusinessCenterOutlinedIcon from "@mui/icons-material/BusinessCenterOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
 
 const AvatarPopper = props => {
   const {
@@ -24,14 +35,17 @@ const AvatarPopper = props => {
     {
       name: "Account",
       path: "/user",
+      icon: AccountCircleOutlinedIcon,
     },
     {
       name: "My Investments",
       path: "/user/dashboard",
+      icon: PaidOutlinedIcon,
     },
     {
       name: "My Projects",
       path: "/projects/dashboard",
+      icon: BusinessCenterOutlinedIcon,
     },
   ];
 
@@ -57,37 +71,37 @@ const AvatarPopper = props => {
                   </Typography>
                 </Box>
                 <Divider />
+                <List>
+                  {/* Page Links */}
+                  {userPages.map((page, i) => (
+                    <ListItem
+                      disablePadding
+                      key={page.path + i}
+                      onClick={() => handleRedirect(page.path)}
+                    >
+                      <ListItemButton>
+                        <ListItemAvatar>
+                          <Avatar>
+                            <page.icon />
+                          </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={page.name} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
 
-                {/* Page Links */}
-                {userPages.map((page, i) => (
-                  <Box
-                    key={page.path + i}
-                    className="popper"
-                    m={1}
-                    onClick={() => handleRedirect(page.path)}
-                  >
-                    <Grid container sx={{ p: 1 }} alignItems="center">
-                      <Grid item xs={12}>
-                        <Box>
-                          <Typography noWrap variant="body1">
-                            {page.name}
-                          </Typography>
-                        </Box>
-                      </Grid>
-                    </Grid>
-                  </Box>
-                ))}
-
-                {/* Logout */}
-                <Box className="popper" m={1} onClick={handleLogout}>
-                  <Grid container sx={{ p: 1 }} alignItems="center">
-                    <Grid item xs={12}>
-                      <Typography noWrap variant="body1">
-                        Logout
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Box>
+                  {/* Logout */}
+                  <ListItem disablePadding onClick={handleLogout}>
+                    <ListItemButton>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <ExitToAppOutlinedIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary={"Logout"} />
+                    </ListItemButton>
+                  </ListItem>
+                </List>
               </Box>
             </Card>
           </Fade>
